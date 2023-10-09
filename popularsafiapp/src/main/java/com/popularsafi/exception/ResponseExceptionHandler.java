18127cdc4a333d28a2b1621b0c35668cf7dbeccc
PR154357 <privera@popular-safi.com>
2023-10-09 13:36:57 -0500

    fuentes de backend

--- popularsafiapp/src/main/java/com/popularsafi/exception/ResponseExceptionHandler.java
+++ popularsafiapp/src/main/java/com/popularsafi/exception/ResponseExceptionHandler.java
@@ -26,27 +26,6 @@ public class ResponseExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(err, HttpStatus.NOT_FOUND);
     }
 
-    //Desde Spring Boot 3
-    /*@ExceptionHandler(ModelNotFoundException.class)
-    public ProblemDetail handleModelNotFoundException(ModelNotFoundException ex, WebRequest request){
-        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
-        problemDetail.setTitle("Model Not Found Exception");
-        problemDetail.setType(URI.create("/not-found"));
-        problemDetail.setProperty("extra1", "extra-value");
-        problemDetail.setProperty("extra2", 32);
-        return problemDetail;
-    }
-
-    @ExceptionHandler(ModelNotFoundException.class)
-    public ErrorResponse handleModelNotFoundException(ModelNotFoundException ex, WebRequest request){
-        return ErrorResponse.builder(ex, HttpStatus.NOT_FOUND, ex.getMessage())
-                .title("Model Not Found")
-                .type(URI.create(request.getContextPath()))
-                .property("extra1", "extra-value")
-                .property("extra2", 32)
-                .build();
-    }*/
-
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
         String msg = ex.getBindingResult().getFieldErrors().stream().map(
